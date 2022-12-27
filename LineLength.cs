b585a24167f3790a3c96c66d7de7cd7b027e5f7d
kumarPraveen9 <gpk50626@gmail.com>
2022-12-27 15:07:52 +0530

    Calculating length of line

--- LineLength.cs
+++ LineLength.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,17 +13,14 @@ namespace LineComparison
         {
             double Leng = 0.0;
             System.Console.WriteLine("Calculating length of two points of x,y Co-ordinates : \n");
-            System.Console.WriteLine("Enter x1 value of first point : ");
-            double x1=Convert.ToDouble(Console.ReadLine());
-            System.Console.WriteLine("Enter y1 value of first point : ");
+            System.Console.WriteLine("Enter x1 y1 x2 y2 value of first point : ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
             double y1 = Convert.ToDouble(Console.ReadLine());
-            System.Console.WriteLine("Enter x2 value of second point : ");
             double x2 = Convert.ToDouble(Console.ReadLine());
-            System.Console.WriteLine("Enter y2 value of second point : ");
             double y2 = Convert.ToDouble(Console.ReadLine());
-            System.Console.WriteLine("\nThe length of two points is: ");
+            
             Leng = Math.Sqrt((Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2)));
-            System.Console.WriteLine(Leng);
+            System.Console.WriteLine("\nThe length of two points is: "+Leng);
         }
     }
 }
