0ff5f08febf03db58555cf8907ede5f9dca06aff
kumarPraveen9 <gpk50626@gmail.com>
2022-12-27 16:02:39 +0530

    checking lengths equal

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcme to Line Comparison Program !!");
-            LineLength.cal_len();
+            //LineLength.cal_len();
+            lineEqual.equal();
         }
     }
 }
